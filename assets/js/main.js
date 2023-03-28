4c24c4e47b9635a403fe8dd46f1079244a464f37
giuseppesciacca <sciaccagi@gmail.com>
2023-03-28 16:26:29 +0200

    [add] img

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 /* Descrizione:
 Stampare a schermo un messaggio all’interno di un h1 utilizzando una proprietá nell'oggetto restituito dalla funzione data.
+
 Bonus:
 Aggiungere alla pagina un’immagine, presa anch’essa da un data. */
 
@@ -9,8 +10,8 @@ createApp({
     data() {
         return {
             texth1: 'Ciao Vue.js!',
-            h1Style: 'text-center p-3'
+            h1Style: 'text-center p-3',
+            urlImg: './assets/img/vue-js-logo-png-transparent-png.png'
         }
     }
 }).mount('#app')
- 
